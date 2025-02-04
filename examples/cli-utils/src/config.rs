297054b0dbdd2bee96554864b4ac903cfd25c674
Phil Lehman <lehmanpm@gmail.com>
2025-02-04 12:03:07 +0000

    added getters and setters for Logging in config.rs module

--- examples/cli-utils/src/config.rs
+++ examples/cli-utils/src/config.rs
@@ -5,6 +5,7 @@
 //! let config = Logging::new();
 //! ```
 //! 
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum LogLevel {
     Debug,
     Info,
@@ -12,6 +13,7 @@ pub enum LogLevel {
     Error,
 }
 
+#[derive(Debug, PartialEq, Clone)]
 pub enum LogOutput {
     Stdout,
     Stderr,
@@ -44,4 +46,92 @@ impl Logging {
             destination: LogOutput::Stdout,
         }
     }
+
+    /// This function enables logging.
+    /// # Examples:
+    /// ```
+    /// use cli_utils::config::{Logging, LogLevel, LogOutput};
+    /// let mut config = Logging::new();
+    /// config.enable();
+    /// ```
+    pub fn enable(&mut self) {
+        self.enabled = true;
+    }
+
+    /// This function disables logging.
+    /// # Examples:
+    /// ```
+    /// use cli_utils::config::{Logging, LogLevel, LogOutput};
+    /// let mut config = Logging::new();
+    /// config.disable();
+    /// ```
+    pub fn disable(&mut self) {
+        self.enabled = false;
+    }
+
+    /// This function sets the log level.
+    /// # Examples:
+    /// ```
+    /// use cli_utils::config::{Logging, LogLevel, LogOutput};
+    /// let mut config = Logging::new();
+    /// config.set_level(LogLevel::Debug);
+    /// ```
+    pub fn set_level(&mut self, level: LogLevel) {
+        self.level = level;
+    }
+
+    /// This function gets the log level.
+    /// # Examples:
+    /// ```
+    /// use cli_utils::config::{Logging, LogLevel, LogOutput};
+    /// let config = Logging::new();
+    /// let level = config.get_level();
+    /// ```
+    pub fn get_level(&self) -> LogLevel {
+        self.level
+    }
+
+    /// This function sets the log output destination.
+    /// # Examples:
+    /// ```
+    /// use cli_utils::config::{Logging, LogLevel, LogOutput};
+    /// let mut config = Logging::new();
+    /// config.set_destination(LogOutput::File("output.log".to_string()));
+    /// ```
+    /// 
+    /// Send the log output to a file:
+    /// ```
+    /// use cli_utils::config::{Logging, LogLevel, LogOutput};
+    /// let mut config = Logging::new();
+    /// config.set_destination(LogOutput::File("output.log".to_string()));
+    /// ```
+    pub fn set_destination(&mut self, destination: LogOutput) {
+        self.destination = destination;
+    }
+
+    /// This function gets the log output destination.
+    /// # Examples:
+    /// ```
+    /// use cli_utils::config::{Logging, LogLevel, LogOutput};
+    /// let config = Logging::new();
+    /// let destination = config.get_destination();
+    /// ```
+    pub fn get_destination(&self) -> String {
+        match self.destination {
+            LogOutput::File(ref path) => {
+                println!("Logging to file: {}", path);
+                path.to_string()
+            }
+            LogOutput::Stderr => {
+                println!("Logging to stderr, the standard error stream");
+                "stderr".to_string()
+            },
+            LogOutput::Stdout => {
+                println!("Logging to stdout, the standard output stream");
+                "stdout".to_string()
+            },
+        }
+    }
+
+
 }
