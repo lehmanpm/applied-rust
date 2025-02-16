613cc85d3a506977ff4deaeea9dbf58e7d715eaf
Phil Lehman <lehmanpm@gmail.com>
2025-02-16 13:38:09 +0000

    Pending changes exported from your codespace

--- examples/cli-utils/tests/test_colors.rs
+++ examples/cli-utils/tests/test_colors.rs
@@ -10,3 +10,36 @@ fn test_red_coloring() {
     color_string.paint();
     assert_eq!(color_string.colorized, "\x1b[31mRed\x1b[0m");
 }
+
+#[test]
+fn test_green_coloring() {
+    let mut color_string = ColorString {
+        color: Color::Green,
+        string: "Green".to_string(),
+        colorized: "".to_string(),
+    };
+    color_string.paint();
+    assert_eq!(color_string.colorized, "\x1b[32mGreen\x1b[0m");
+}
+
+#[test]
+fn test_blue_coloring() {
+    let mut color_string = ColorString {
+        color: Color::Blue,
+        string: "Blue".to_string(),
+        colorized: "".to_string(),
+    };
+    color_string.paint();
+    assert_eq!(color_string.colorized, "\x1b[34mBlue\x1b[0m");
+}
+
+#[test]
+fn test_bold_coloring() {
+    let mut color_string = ColorString {
+        color: Color::Bold,
+        string: "Yellow".to_string(),
+        colorized: "".to_string(),
+    };
+    color_string.paint();
+    assert_eq!(color_string.colorized, "\x1b[1mYellow\x1b[0m");
+}
